3e42a0347138291c3517ca6a37123060300c2641
CorneHQ <cornedev@outlook.com>
2020-06-07 12:44:51 +0200

    Update skip.js

--- src/commands/skip.js
+++ src/commands/skip.js
@@ -5,7 +5,7 @@ module.exports = {
 	execute(message) {
 		const { channel } = message.member.voice;
 		if (!channel) return message.channel.send('I\'m sorry but you need to be in a voice channel to play music!');
-		if (!message.member.roles.cache.has('601505636496310273')) return message.channel.send('You need to be staff to skip numbers!');
+		if (!message.member.roles.cache.has('601505636496310273')) return message.channel.send('You need to be staff to skip songs!');
 		const serverQueue = message.client.queue.get(message.guild.id);
 		if (!serverQueue) return message.channel.send('There is nothing playing that I could skip for you.');
 		serverQueue.connection.dispatcher.end('Skip command has been used!');
